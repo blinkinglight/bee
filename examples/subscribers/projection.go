d3fdc3d50954e6aa5666c4ee4093a1a3cdac8a11
M <mz@9g.lt>
2025-06-17 09:41:30 +0300

    cleanup

--- examples/subscribers/projection.go
+++ examples/subscribers/projection.go
@@ -44,26 +44,3 @@ func (p UserProjection) Query(query *gen.QueryEnvelope) (any, error) {
 		return nil, nil
 	}
 }
-
-// func runProjection(js nats.JetStreamContext) {
-// 	// projection for now to console
-// 	js.Subscribe("events.users.>", func(m *nats.Msg) {
-// 		var event gen.EventEnvelope
-// 		proto.Unmarshal(m.Data, &event)
-// 		log.Printf("got event on subject %s %s", m.Subject, event.Payload)
-
-// 		switch event.EventType {
-// 		case "created":
-// 			user, _ := bee.Unmarshal[User](event.Payload)
-// 			println("User created:", user.Name, "from", user.Country)
-// 		case "updated":
-// 			user, _ := bee.Unmarshal[User](event.Payload)
-// 			println("User updated:", user.Name, "from", user.Country)
-// 		case "deleted":
-// 			println("User deleted with ID:", event.AggregateId)
-// 		default:
-// 			println("Unknown event type:", event.EventType)
-// 		}
-
-// 	}, nats.Durable("vienas"))
-// }
